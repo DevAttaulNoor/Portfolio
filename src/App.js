3e076b807db776eb334b22dc230d7d12f72a4b17
Atta ul Noor <attaulnoor2001@gmail.com>
2023-08-12 00:54:10 +0500

    Modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
 import './App.css';
+import 'animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <Banner /> */}
+      <Banner />
     </div>
   );
 }
