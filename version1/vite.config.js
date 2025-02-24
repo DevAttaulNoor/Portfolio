601a84d67c556b29b7c461539cf72dc3b3e219b0
Atta ul Noor <attaulnoor2001@gmail.com>
2025-02-24 21:16:26 +0500

    Update workflow and Vite config for GitHub Pages deployment

--- version1/vite.config.js
+++ version1/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Portfolio/version1",
+  build: { outDir: 'build' },
   plugins: [react()],
 })
